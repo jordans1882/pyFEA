db781d78a9d64602e9efcdc6f52c474b7ad3c58a
NickyTurk <turknicky@gmail.com>
2024-06-14 18:34:46 -0400

    docstrings and fixed DE crossover

--- feareu/base_algos/fea_de.py
+++ feareu/base_algos/fea_de.py
@@ -3,14 +3,35 @@ from feareu.base_algos.fea_base_algo import FeaBaseAlgo
 import numpy as np
 
 class FeaDE(DE, FeaBaseAlgo):
+    """
+    The Differential Evolution algorithm set up for use in an FEA.
+    See the base Differential Evolution algorithm in de.py for the full functionality.
+    """
     def get_solution_at_index(self, idx):
+        """
+        The method to retrieve a variable's value from the base algorithm.
+        @param idx: the index of the variable to be retrieved.
+        """
         return self.best_solution[idx]
     
     def update_worst(self, context):
+        """
+        The second half of the share step in the FEA algorithm, where we update 
+        the worst individual in our population to become the context vector.
+        @param context: the context vector of our FEA.
+        """
         self.pop[np.argmax(self.pop_eval), :] = (context)
 
     @classmethod
     def from_kwargs(cls, function, domain, params):
+        """
+        The method for inputting parameters to the DE from FEA.
+        @param function: the objective function to minimize.
+        @param domain: the domain over which to minimize as a numpy array of size (dim, 2)
+        @param params: the remaining parameters, namely generations, pop_size, mutation_factor,
+        and crossover_rate. These are taken in as a dictionary here from the keyword arguments
+        passed to FEA's constructor.
+        """
         kwargs = {
             "generations": 100,
             "pop_size": 20,
@@ -27,16 +48,22 @@ class FeaDE(DE, FeaBaseAlgo):
             crossover_rate=kwargs["crossover_rate"],
         )
     def base_reset(self):
-        self.pop_eval = [self.func(self.pop[i, :]) for i in range(self.pop_size)]
-        self.best_solution = np.copy(self.pop[np.argmin(self.pop_eval), :])
-        self.best_eval = np.min(self.pop_eval)
+        """
+        Reset the algorithm in preparation for another run. For this algorithm, 
+        this is currently indistinguishable from an update_bests run.
+        """
+        self.update_bests()
 
     def run(self):
+        """
+        Run the base algorithm.
+        """
         return super().run()
 
     def update_bests(self):
-        for pidx in range(self.pop_size):
-            curr_eval = self.func(self.pop[pidx, :])
-            self.pop_eval[pidx] = curr_eval
-        self.best_eval = np.min(self.pop_eval)
+        """
+        Update the evaluation of the objective function after a context vector update.
+        """
+        self.pop_eval = [self.func(self.pop[i, :]) for i in range(self.pop_size)]
         self.best_solution = np.copy(self.pop[np.argmin(self.pop_eval), :])
+        self.best_eval = np.min(self.pop_eval)
