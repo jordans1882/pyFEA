0f877f73ee56cb561c048f9fcf64d02e6eb3649b
Eve <adk6dr@virginia.edu>
2024-06-28 16:16:53 -0600

    fixed the bspline benchmarks

---
+++ examples/checking_spline_test_accuracy.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+#from feareu.experiments.general_fea_experiments import big_spike, cliff, discontinuity, doppler, smooth_peak, second_smooth_peak
+from feareu import big_spike, cliff, discontinuity, doppler, smooth_peak, second_smooth_peak
+import numpy as np
+
+num_points = 500
+irange = np.arange(0, num_points)
+
+"""
+big_spike_ys = []
+for i in irange:
+    big_spike_ys.append(big_spike(i/num_points))
+plt.scatter(irange, big_spike_ys)
+"""
+"""
+cliff_ys = []
+for i in irange:
+    cliff_ys.append(cliff(i/num_points))
+plt.scatter(irange, cliff_ys)
+"""
+"""
+discontinuity_ys = []
+for i in irange:
+    discontinuity_ys.append(discontinuity(i/num_points))
+plt.scatter(irange, discontinuity_ys)
+"""
+"""
+doppler_ys = []
+for i in irange:
+    doppler_ys.append(doppler(i/num_points))
+plt.scatter(irange, doppler_ys)
+"""
+
+peak_range = np.arange(-num_points/2, num_points/2)
+"""
+smooth_peak_ys = []
+for i in peak_range:
+    smooth_peak_ys.append(smooth_peak(2*i/num_points))
+plt.scatter(peak_range, smooth_peak_ys)
+"""
+"""
+second_smooth_peak_ys = []
+for i in peak_range:
+    second_smooth_peak_ys.append(second_smooth_peak(2*i/num_points))
+plt.scatter(peak_range, second_smooth_peak_ys)
+"""
+plt.grid(True)
+plt.show()
--- feareu/experiments/general_fea_experiments/bspline_benchmarks.py
+++ feareu/experiments/general_fea_experiments/bspline_benchmarks.py
@@ -12,10 +12,10 @@ def discontinuity(x):
     return np.where(x < 0.6, 1/(0.01+(x-0.3)**2), 1/(0.015+(x-0.65)**2))
 
 def smooth_peak(x):
-    return np.sin(x) + 2 * np.exp(-30 * x**2)
+    return np.sin(x) + (2 * np.e) ** (-30 * x**2)
 
 def second_smooth_peak(x):
-    return np.sin(2*x) + 2 * np.exp(-16 * x**2) + 2
+    return np.sin(2*x) + (2 * np.e) ** (-16 * x**2) + 2
 
 def doppler(x):
     return np.sin(20/(x+0.15))
