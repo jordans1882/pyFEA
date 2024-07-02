a64907d4cbb31c6271721cf293408511b2487b7c
NickyTurk <turknicky@gmail.com>
2024-07-02 11:39:45 -0400

    complete data loading for diagnostic plots

--- feareu/experiments/bspline_experiments/diagnostic_plots.py
+++ feareu/experiments/bspline_experiments/diagnostic_plots.py
@@ -31,9 +31,9 @@ def construct_alg(base_alg, parameters):
 if __name__ == "__main__":
     benchmarks = [feareu.big_spike, feareu.discontinuity, feareu.cliff, feareu.smooth_peak, feareu.second_smooth_peak, feareu.doppler]
     for func, benchmark in enumerate(benchmarks):
-        for base_alg in alg_list:
-            for sample_size in sample_sizes:
-                for noise_level in range(6):
+        for sample_size in sample_sizes:
+            for noise_level in range(6):
+                for base_alg in alg_list:
                     x = np.random.random(sample_size)
                     y = benchmark(x)
                     func_width = np.max(y) - np.min(y)
@@ -43,8 +43,10 @@ if __name__ == "__main__":
                     fitness = bspline_eval_class(x, y)
                     storage = open(f'results/function{func}_{base_alg}_sample{sample_size}_noise{noise_level}', 'rb')
                     data = pickle.load(storage)
-                    alg = construct_alg(base_alg, data['params'])
+                    params = data['params']
+                    alg = construct_alg(base_alg, params)
                     storage.close()
+                    print("running ", base_alg, " with params: ", params)
                     alg.run()
                     x = np.random.random(sample_size)
                     y = benchmark(x)
@@ -55,24 +57,52 @@ if __name__ == "__main__":
                     storage = open(f'results/FEA_function{func}_{base_alg}_sample{sample_size}_noise{noise_level}', 'rb')
                     data = pickle.load(storage)
                     params = data['params']
-                    factors = feareu.linear_factorizer(fact_size=params['fact_size'], overlap=params['overlap'], dim=params['dim'])
-                    alg = fea(
-                            factors,
-                            fitness,
-                            iterations=params['iterations'],
-                            dim=params['dim'],
-                            base_alg=base_alg,
-                            domain=(0,1),
-                            diagnostics_amount=1,
-                            pop_size=params['pop_size'],
-                            generations=params['generations'],
-                            mutation_factor=params['mutation_factor'],
-                            crossover_rate=params['crossover_rate'],
-                            mutation_rate=params['mutation_rate'],
-                            mutation_range=params['mutation_range'],
-                            phi_p=params['phi_p'],
-                            phi_g=params['phi_g'],
-                            omega=params['omega'],
-                            )
+                    factors = feareu.linear_factorizer(fact_size=int(params['fact_size']), overlap=int(params['overlap']), dim=int(params['dim']))
+                    try:
+                        alg = fea(
+                                factors,
+                                fitness,
+                                iterations=int(params['iterations']),
+                                dim=int(params['dim']),
+                                base_algo_name=base_alg,
+                                domain=(0,1),
+                                diagnostics_amount=1,
+                                pop_size=int(params['pop_size']),
+                                generations=int(params['generations']),
+                                phi_p=params['phi_p'],
+                                phi_g=params['phi_g'],
+                                omega=params['omega'],
+                                )
+                    except:
+                        try:
+                            alg = fea(
+                                factors,
+                                fitness,
+                                iterations=int(params['iterations']),
+                                dim=int(params['dim']),
+                                base_algo_name=base_alg,
+                                domain=(0,1),
+                                diagnostics_amount=1,
+                                pop_size=int(params['pop_size']),
+                                generations=int(params['generations']),
+                                mutation_factor=params['mutation_factor'],
+                                crossover_rate=params['crossover_rate'],
+                                )
+                        except:
+                            alg = fea(
+                                factors,
+                                fitness,
+                                iterations=int(params['iterations']),
+                                dim=int(params['dim']),
+                                base_algo_name=base_alg,
+                                domain=(0,1),
+                                diagnostics_amount=1,
+                                pop_size=int(params['pop_size']),
+                                generations=int(params['generations']),
+                                mutation_rate=params['mutation_rate'],
+                                mutation_range=params['mutation_range'],
+                                )
+
                     storage.close()
+                    print("running FEA ", base_alg, " with params: ", params)
                     alg.run()
