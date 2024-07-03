4b7a1331ba4b9bc28755480a333680945e196e63
jordans1882 <jordans1882@gmail.com>
2024-07-03 09:18:40 -0600

    Add bspline fea pso example

--- examples/FEATesting/bspline_fea_pso_example.py
+++ examples/FEATesting/bspline_fea_pso_example.py
@@ -2,9 +2,34 @@ import time
 
 import matplotlib.pyplot as plt
 import numpy as np
+from cmakeswig.pydatamunge import pydatamunge as dm
 
 import feareu
 from feareu import BsplineFeaPSO, ParallelBsplineFeaPSO, SlowBsplineEval, doppler
+
+# dm.GSLSLM()
+
+
+class FasterBsplineEval:
+    def __init__(self, x, y):
+        self.x = dm.Vector(x)
+        self.y = dm.Vector(y)
+
+    def __call__(self, knots):
+        tx = dm.Vector(self.x)
+        ty = dm.Vector(self.y)
+        knotvec = dm.Vector(knots)
+        start_time = time.time()
+        xmat = dm.gsl_bspline_eval(tx, knotvec, 3, False)
+        end_time = time.time()
+        print("fill mat took", end_time - start_time)
+
+        start_time = time.time()
+        slm = dm.GSLSLM(xmat, ty)
+        end_time = time.time()
+        print("fitting took", end_time - start_time)
+        return slm.get_mse()
+
 
 sample_size = 100000
 x = np.random.random(sample_size)
@@ -20,22 +45,36 @@ plt.scatter(x, y)
 plt.show()
 
 
-start_time = time.time()
 benchmark = SlowBsplineEval(x, y)
+start_time = time.time()
+benchmark(kseq)
 end_time = time.time()
 print(end_time - start_time)
 
+benchmark = FasterBsplineEval(x, y)
+start_time = time.time()
 benchmark(kseq)
+end_time = time.time()
+print(end_time - start_time)
 
 
 dom = np.zeros((k, 2))
 dom[:, 1] = 1.0
 
 # Test base PSO works (DONE! it works?)
-fea = BsplineFeaPSO(benchmark, dom)
-fea = ParallelBsplineFeaPSO(benchmark, dom, pop_size=80, processes=12)
+fea = ParallelBsplineFeaPSO(benchmark, dom, pop_size=40, processes=12)
+
 fea.run()
 fea.gbest
 
+fea.diagnostic_plots()
+plt.show()
+
+
+plt.hist(fea.gbest, bins=10, color="skyblue", edgecolor="black")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.title("Histogram of Data")
+plt.show()
 
 ParallelBsplineFEA
