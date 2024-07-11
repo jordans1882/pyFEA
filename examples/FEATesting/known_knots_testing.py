094241131554983dfdd5fc70ae3e7fdd7856f7f8
Eve <adk6dr@virginia.edu>
2024-07-11 15:28:29 -0600

    known knots for pso

--- examples/FEATesting/known_knots_testing.py
+++ examples/FEATesting/known_knots_testing.py
@@ -1,4 +1,9 @@
 from feareu.benchmarks.known_knots_fea import KnownKnotsFea
 from feareu.base_algos.bspline_specific.bspline_fea_pso import BsplineFeaPSO
-tester = KnownKnotsFea(number_of_knots=12, number_of_points=1000, max_error=0.01, delta=0.01, base_algo=BsplineFeaPSO, diagnostics_amount=1, generations=10, pop_size=15)
-tester.run()
+from feareu.benchmarks.known_knots_pso import KnownKnotsPso
+
+"""fea_tester = KnownKnotsFea(number_of_knots=12, number_of_points=1000, max_error=0.01, delta=0.01, base_algo=BsplineFeaPSO, diagnostics_amount=1, generations=10, pop_size=15)
+fea_tester.run()"""
+
+pso_tester = KnownKnotsPso(number_of_knots=10, factor_size=3, overlap=1, number_of_points=1000, max_error=0.01, delta=0.01, diagnostics_amount=1, pop_size=200)
+pso_tester.run()
