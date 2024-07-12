c60afd9cd8aa3fbc7b6b80081ebb73aa3002c869
jordans1882 <jordans1882@gmail.com>
2024-07-12 10:57:04 -0600

    Move base algos

--- pyfea/base_algos/__init__.py
+++ pyfea/base_algos/__init__.py
@@ -1,18 +1,23 @@
-from .traditional.pso import PSO
-from .traditional.fea_pso import FeaPso
-from .traditional.ga import GA
-from .traditional.fea_ga import FeaGA
-from .traditional.de import DE
-from .traditional.fea_de import FeaDE
-from .traditional.parallel_evaluation import parallel_eval
-from .traditional.parallel_fea_pso import ParallelFeaPSO
-from .traditional.parallel_fea_de import ParallelFeaDE
-from .traditional.parallel_fea_ga import ParallelFeaGA
-from .bspline_specific.bspline_fea_pso import BsplineFeaPSO
-from .bspline_specific.bspline_fea_ga import BsplineFeaGA
-from .bspline_specific.bspline_fea_de import BsplineFeaDE
-from .bspline_specific.parallel_bspline_fea_pso import ParallelBsplineFeaPSO
-from .bspline_specific.parallel_bspline_fea_de import ParallelBsplineFeaDE
-from .bspline_specific.parallel_bspline_fea_ga import ParallelBsplineFeaGA
+from .de import DE
+from .fea_de import FeaDE
+from .fea_ga import FeaGA
+from .fea_pso import FeaPso
+from .ga import GA
+from .parallel_evaluation import parallel_eval
+from .parallel_fea_de import ParallelFeaDE
+from .parallel_fea_ga import ParallelFeaGA
+from .parallel_fea_pso import ParallelFeaPSO
+from .pso import PSO
 
-__all__ = ["PSO", "FeaPso", "DE", "FeaDE", "GA", "FeaGA", "parallel_eval", "ParallelFeaPSO", "ParallelFeaDE", "ParallelFeaGA", "BsplineFeaPSO", "BsplineFeaGA", "BsplineFeaDE", "ParallelBsplineFeaPSO", "ParallelBsplineFeaDE", "ParallelBsplineFeaGA"]
+__all__ = [
+    "DE",
+    "FeaDE",
+    "FeaGA",
+    "FeaPso",
+    "GA",
+    "PSO",
+    "ParallelFeaDE",
+    "ParallelFeaGA",
+    "ParallelFeaPSO",
+    "parallel_eval",
+]
