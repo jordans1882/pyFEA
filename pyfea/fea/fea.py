e202b3ea2a81f3cb1d5b15b8799947679cc4c21c
NickyTurk <turknicky@gmail.com>
2024-07-24 18:19:15 -0400

    fix syntax error

--- pyfea/fea/fea.py
+++ pyfea/fea/fea.py
@@ -11,7 +11,7 @@ from pyfea.fea.function import Function
 class FEA:
     """Factored Evolutionary Architecture, implemented based on the 2017 paper by Strasser et al."""
 
-    def __init__(self, factors, function, iterations, dim, base_algo, domain, terminate_by_fitness=False, **kwargs=None):
+    def __init__(self, factors, function, iterations, dim, base_algo, domain, terminate_by_fitness=False, **kwargs):
         """
         @param factors: list of lists, contains the dimensions that each factor of the architecture optimizes over.
         @param function: the objective function that the FEA minimizes.
